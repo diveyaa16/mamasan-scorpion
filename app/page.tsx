e1679ec9dda69bfb0c9d97a9573d6ef437be0041
diveyaa16 <diyaravi16@gmail.com>
2026-05-12 19:44:57 +0800

    remove duplicates

--- app/page.tsx
+++ app/page.tsx
@@ -264,7 +264,6 @@ export default function Home() {
             "Signature Rotty (4 PCS)",
             "Snacks",
             "Fried Rice & Noodles",
-            "Snacks",
             "Milkshakes",
             "Protein Shakes",
             "Fresh Juices",
